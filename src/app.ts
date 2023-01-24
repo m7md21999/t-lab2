0f702b5146e23c255c770b61fd30d7ee52369d8d
Mohammed <m7md21999@gmail.com>
2023-01-24 19:40:52 +0300

    Added array route

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,25 @@ app.get('/hey', (req:Request, res:Response, next:NextFunction) => {
     res.send('Hey from Express');
   });
 
+// Print Array
+  app.get('/array', (req:Request, res:Response, next:NextFunction) => {
+    res.send(Members);
+  });
+
+
+  // Dummmy data - Array
+  type Member = {
+    id:number,
+    username:string,
+    password:string
+  };
+
+  let Members:Member[] = [
+    {id:1,username:"Mohamed",password:"Aa@12345"},
+    {id:2,username:"nasser",password:"Aa@1111"},
+    {id:3,username:"Majed",password:"Aa@00000"},
+
+  ];
 
   app.listen(port, () => {
     console.log(`app listening on port ${port}`)
